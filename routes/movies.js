35b35d6643eac22a426a0824db0028e7098f1ea7
Marco Stellin <letal32@gmail.com>
2018-05-12 01:39:31 +0100

    Show movie route and view

--- routes/movies.js
+++ routes/movies.js
@@ -4,6 +4,12 @@ var router = express.Router();
 /* GET index  */
 router.get('/', function ( req, res ){
     res.render('movies/index');
-}); 
+});
+
+router.get ('/:id', function (req, res) {
+    res.locals.baseRequest(`/movie/${req.params.id}?api_key=${res.locals.apiKey}&language=en-US`, function (err, response, body){
+        res.render('movies/show', {movie: body});
+     });
+});
 
 module.exports = router;
