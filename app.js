3e60b992420ae277bc1aef523ca08e42a485599e
Marco Stellin <letal32@gmail.com>
2018-05-14 00:25:54 +0100

    Updated syntax to es6

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ app.use('/', indexRoute);
 app.use('/movies', movieRoute);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use( (req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use( (err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -55,6 +55,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port " + process.env.PORT);
 });
