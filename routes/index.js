500bba202b6a7de87cdfae53656624cd2fa4b822
Marco Stellin <letal32@gmail.com>
2018-06-02 18:49:35 +0100

    Signup routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const request = require('request');
+const passport = require('passport');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index');
 });
+
+/* GET signup page */
+router.get ('/signup', (req, res) => {
+  res.render('signup');
+});
+
+/* POST new user */
+router.post('/signup', passport.authenticate ('signup', {
+  successRedirect: '/',
+  failureRedirect: '/signup'
+}));
 
 module.exports = router;
