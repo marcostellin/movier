1697939ca9da4383305266400c9126665e1b874c
Marco Stellin <letal32@gmail.com>
2018-12-23 17:58:27 +0100

    Fix bug text not showing in notification if display none

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,7 +5,7 @@ const flashSuccess = document.querySelector('#flash-success');
 alertify.set('notifier','position', 'bottom-center');
 
 if (flashError && flashError.innerText.length > 0){
-    alertify.notify(flashError, 'error', 5, function(){  console.log('dismissed'); });
+    alertify.notify(flashError.innerText, 'error', 5, function(){  console.log('dismissed'); });
 } else if (flashSuccess && flashSuccess.innerText.length > 0) {
-    alertify.notify(flashSuccess, 'success', 5, function(){  console.log('dismissed'); })
+    alertify.notify(flashSuccess.innerText, 'success', 5, function(){  console.log('dismissed'); });
 }
